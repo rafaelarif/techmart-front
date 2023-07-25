5fa3adc387469fd03274bb1923aefd57c5f21170
Rafael Arif <rafael.arif@outlook.com>
2023-07-25 03:03:06 -0400

    final final fix

--- pages/category/[id].js
+++ pages/category/[id].js
@@ -94,7 +94,7 @@ export default function CategoryPage({
     return (
         <>
             <Head>
-                <title>Cart - TechMart</title>
+                <title>{category.name}s - TechMart</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
             <Header />
